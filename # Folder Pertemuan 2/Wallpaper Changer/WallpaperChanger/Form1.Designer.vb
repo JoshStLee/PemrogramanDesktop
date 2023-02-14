fc372d75614cdc07f73c5dc4ed005c76720bb175
Joshua Lee <joshualie1999@gmail.com>
2023-02-14 13:48:21 +0700

    HOL2, Wallpaper Changer + Jawaban Pertanyaan

---
+++ # Folder Pertemuan 2/Wallpaper Changer/WallpaperChanger/Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GarisBatasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblPath = New System.Windows.Forms.Label()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusReady = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusCurrent = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.TimerWall = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerElapsed = New System.Windows.Forms.Timer(Me.components)
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.BtnBrowse = New System.Windows.Forms.Button()
+        Me.clbWallpaper = New System.Windows.Forms.CheckedListBox()
+        Me.lblInterval = New System.Windows.Forms.Label()
+        Me.cbPreview = New System.Windows.Forms.CheckBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.cbInterval = New System.Windows.Forms.ComboBox()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.GarisBatasToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'GarisBatasToolStripMenuItem
+        '
+        Me.GarisBatasToolStripMenuItem.Name = "GarisBatasToolStripMenuItem"
+        Me.GarisBatasToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.GarisBatasToolStripMenuItem.Text = "Garis Batas"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpToolStripMenuItem1, Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'HelpToolStripMenuItem1
+        '
+        Me.HelpToolStripMenuItem1.Name = "HelpToolStripMenuItem1"
+        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.HelpToolStripMenuItem1.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'lblPath
+        '
+        Me.lblPath.AutoSize = True
+        Me.lblPath.Location = New System.Drawing.Point(36, 45)
+        Me.lblPath.Name = "lblPath"
+        Me.lblPath.Size = New System.Drawing.Size(67, 15)
+        Me.lblPath.TabIndex = 1
+        Me.lblPath.Text = "Folder Path"
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(122, 42)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(523, 23)
+        Me.txtPath.TabIndex = 2
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusReady, Me.ToolStripStatusCurrent})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusReady
+        '
+        Me.ToolStripStatusReady.Name = "ToolStripStatusReady"
+        Me.ToolStripStatusReady.Size = New System.Drawing.Size(39, 17)
+        Me.ToolStripStatusReady.Text = "Ready"
+        '
+        'ToolStripStatusCurrent
+        '
+        Me.ToolStripStatusCurrent.Name = "ToolStripStatusCurrent"
+        Me.ToolStripStatusCurrent.Size = New System.Drawing.Size(138, 17)
+        Me.ToolStripStatusCurrent.Text = "Current Wallpaper: None"
+        '
+        'TimerWall
+        '
+        '
+        'TimerElapsed
+        '
+        Me.TimerElapsed.Interval = 1000
+        '
+        'BtnBrowse
+        '
+        Me.BtnBrowse.Location = New System.Drawing.Point(676, 41)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.BtnBrowse.TabIndex = 4
+        Me.BtnBrowse.Text = "..."
+        Me.BtnBrowse.UseVisualStyleBackColor = True
+        '
+        'clbWallpaper
+        '
+        Me.clbWallpaper.FormattingEnabled = True
+        Me.clbWallpaper.Location = New System.Drawing.Point(36, 82)
+        Me.clbWallpaper.Name = "clbWallpaper"
+        Me.clbWallpaper.Size = New System.Drawing.Size(370, 328)
+        Me.clbWallpaper.TabIndex = 5
+        '
+        'lblInterval
+        '
+        Me.lblInterval.AutoSize = True
+        Me.lblInterval.Location = New System.Drawing.Point(441, 359)
+        Me.lblInterval.Name = "lblInterval"
+        Me.lblInterval.Size = New System.Drawing.Size(46, 15)
+        Me.lblInterval.TabIndex = 6
+        Me.lblInterval.Text = "Interval"
+        '
+        'cbPreview
+        '
+        Me.cbPreview.AutoSize = True
+        Me.cbPreview.Checked = True
+        Me.cbPreview.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.cbPreview.Location = New System.Drawing.Point(441, 82)
+        Me.cbPreview.Name = "cbPreview"
+        Me.cbPreview.Size = New System.Drawing.Size(103, 19)
+        Me.cbPreview.TabIndex = 7
+        Me.cbPreview.Text = "Preview Image"
+        Me.cbPreview.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(441, 107)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(310, 249)
+        Me.Panel1.TabIndex = 8
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(300, 239)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'cbInterval
+        '
+        Me.cbInterval.FormattingEnabled = True
+        Me.cbInterval.Items.AddRange(New Object() {"10 sec", "30 sec", "1 min"})
+        Me.cbInterval.Location = New System.Drawing.Point(524, 357)
+        Me.cbInterval.Name = "cbInterval"
+        Me.cbInterval.Size = New System.Drawing.Size(121, 23)
+        Me.cbInterval.TabIndex = 9
+        Me.cbInterval.Text = "Select First"
+        '
+        'btnRun
+        '
+        Me.btnRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRun.Location = New System.Drawing.Point(671, 357)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 10
+        Me.btnRun.Text = "Run!"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.cbInterval)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.cbPreview)
+        Me.Controls.Add(Me.lblInterval)
+        Me.Controls.Add(Me.clbWallpaper)
+        Me.Controls.Add(Me.BtnBrowse)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.lblPath)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Wallpaper Changer"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GarisBatasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents lblPath As Label
+    Friend WithEvents txtPath As TextBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusReady As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusCurrent As ToolStripStatusLabel
+    Friend WithEvents TimerWall As Timer
+    Friend WithEvents TimerElapsed As Timer
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents BtnBrowse As Button
+    Friend WithEvents clbWallpaper As CheckedListBox
+    Friend WithEvents lblInterval As Label
+    Friend WithEvents cbPreview As CheckBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents cbInterval As ComboBox
+    Friend WithEvents btnRun As Button
+End Class
