4b665323067b97cbd0e8a52d2f5581b31d7d0c50
Joshua Lee <joshualie1999@gmail.com>
2023-03-21 16:05:51 +0700

    SIParkir Pertemuan 7

---
+++ # Folder Pertemuan 7/SIParkir/Form4.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLangganan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtNoPlat = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBiaya = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.cmbJenis = New System.Windows.Forms.ComboBox()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.BtnEdit = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.id = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.no_plat = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nama = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.biaya = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.jenis = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.expired = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.lblId = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(41, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(181, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nama Pelanggan"
+        '
+        'txtNama
+        '
+        Me.txtNama.Enabled = False
+        Me.txtNama.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNama.Location = New System.Drawing.Point(238, 59)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(365, 26)
+        Me.txtNama.TabIndex = 1
+        '
+        'txtNoPlat
+        '
+        Me.txtNoPlat.Enabled = False
+        Me.txtNoPlat.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNoPlat.Location = New System.Drawing.Point(238, 105)
+        Me.txtNoPlat.Name = "txtNoPlat"
+        Me.txtNoPlat.Size = New System.Drawing.Size(189, 32)
+        Me.txtNoPlat.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(41, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 26)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "No Plat"
+        '
+        'txtBiaya
+        '
+        Me.txtBiaya.Enabled = False
+        Me.txtBiaya.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBiaya.Location = New System.Drawing.Point(238, 143)
+        Me.txtBiaya.Name = "txtBiaya"
+        Me.txtBiaya.Size = New System.Drawing.Size(189, 32)
+        Me.txtBiaya.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(41, 149)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(168, 26)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Tarif Langganan"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(41, 187)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(161, 26)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Berlaku Hingga"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(238, 188)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(41, 19)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(144, 26)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "ID Pelanggan"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(470, 119)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(31, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Jenis"
+        '
+        'cmbJenis
+        '
+        Me.cmbJenis.Enabled = False
+        Me.cmbJenis.FormattingEnabled = True
+        Me.cmbJenis.Items.AddRange(New Object() {"", "Motor", "Mobil", "Taxi/Umum", "Bis/Truk", "Sepeda"})
+        Me.cmbJenis.Location = New System.Drawing.Point(507, 116)
+        Me.cmbJenis.Name = "cmbJenis"
+        Me.cmbJenis.Size = New System.Drawing.Size(121, 21)
+        Me.cmbJenis.TabIndex = 10
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Location = New System.Drawing.Point(473, 188)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(75, 23)
+        Me.btnTambah.TabIndex = 11
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(635, 188)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(75, 23)
+        Me.btnHapus.TabIndex = 12
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'BtnEdit
+        '
+        Me.BtnEdit.Location = New System.Drawing.Point(554, 188)
+        Me.BtnEdit.Name = "BtnEdit"
+        Me.BtnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.BtnEdit.TabIndex = 13
+        Me.BtnEdit.Text = "Edit"
+        Me.BtnEdit.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.id, Me.no_plat, Me.nama, Me.biaya, Me.jenis, Me.expired})
+        Me.DataGridView1.Location = New System.Drawing.Point(46, 256)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(664, 150)
+        Me.DataGridView1.TabIndex = 14
+        '
+        'id
+        '
+        Me.id.HeaderText = "Id"
+        Me.id.Name = "id"
+        '
+        'no_plat
+        '
+        Me.no_plat.HeaderText = "No Plat"
+        Me.no_plat.Name = "no_plat"
+        '
+        'nama
+        '
+        Me.nama.HeaderText = "Nama"
+        Me.nama.Name = "nama"
+        '
+        'biaya
+        '
+        Me.biaya.HeaderText = "Biaya"
+        Me.biaya.Name = "biaya"
+        '
+        'jenis
+        '
+        Me.jenis.HeaderText = "Jenis"
+        Me.jenis.Name = "jenis"
+        '
+        'expired
+        '
+        Me.expired.HeaderText = "Expired"
+        Me.expired.Name = "expired"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(46, 233)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(249, 20)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Kendaraan Terdaftar Langganan"
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(635, 217)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 16
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        Me.btnCancel.Visible = False
+        '
+        'lblId
+        '
+        Me.lblId.AutoSize = True
+        Me.lblId.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblId.Location = New System.Drawing.Point(233, 19)
+        Me.lblId.Name = "lblId"
+        Me.lblId.Size = New System.Drawing.Size(144, 26)
+        Me.lblId.TabIndex = 8
+        Me.lblId.Text = "ID Pelanggan"
+        '
+        'frmLangganan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(761, 450)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.BtnEdit)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnTambah)
+        Me.Controls.Add(Me.cmbJenis)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblId)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtBiaya)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtNoPlat)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmLangganan"
+        Me.Text = "SIParkir - Langganan"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtNoPlat As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtBiaya As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents cmbJenis As ComboBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents BtnEdit As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents id As DataGridViewTextBoxColumn
+    Friend WithEvents no_plat As DataGridViewTextBoxColumn
+    Friend WithEvents nama As DataGridViewTextBoxColumn
+    Friend WithEvents biaya As DataGridViewTextBoxColumn
+    Friend WithEvents jenis As DataGridViewTextBoxColumn
+    Friend WithEvents expired As DataGridViewTextBoxColumn
+    Friend WithEvents lblId As Label
+End Class
---
+++ # Folder Pertemuan 7/SIParkir/Form5.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form5
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.btnTampil = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.btnTutup = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Periode"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(111, 33)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 1
+        Me.DateTimePicker1.Value = New Date(2023, 3, 14, 14, 31, 15, 0)
+        '
+        'btnTampil
+        '
+        Me.btnTampil.Location = New System.Drawing.Point(338, 34)
+        Me.btnTampil.Name = "btnTampil"
+        Me.btnTampil.Size = New System.Drawing.Size(75, 23)
+        Me.btnTampil.TabIndex = 2
+        Me.btnTampil.Text = "Tampilkan"
+        Me.btnTampil.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(47, 83)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(366, 335)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'btnTutup
+        '
+        Me.btnTutup.Location = New System.Drawing.Point(369, 468)
+        Me.btnTutup.Name = "btnTutup"
+        Me.btnTutup.Size = New System.Drawing.Size(75, 23)
+        Me.btnTutup.TabIndex = 4
+        Me.btnTutup.Text = "Tutup"
+        Me.btnTutup.UseVisualStyleBackColor = True
+        '
+        'Form5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(465, 513)
+        Me.Controls.Add(Me.btnTutup)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.btnTampil)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form5"
+        Me.Text = "SIParkir - Laporan Pendapatan Parkir"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents btnTampil As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents btnTutup As Button
+End Class
