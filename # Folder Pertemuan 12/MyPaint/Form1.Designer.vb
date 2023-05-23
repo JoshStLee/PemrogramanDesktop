fc88d893aa9e2d6ff71b240ffa13c7d4f2df1bae
Joshua Lee <joshualie1999@gmail.com>
2023-05-23 17:07:01 +0700

    Tugas 4

---
+++ # Folder Pertemuan 12/MyPaint/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CBoxMode = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CBoxUkuran = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BtnBersihkan = New System.Windows.Forms.Button()
+        Me.Warna1 = New System.Windows.Forms.Panel()
+        Me.Warna2 = New System.Windows.Forms.Panel()
+        Me.Warna3 = New System.Windows.Forms.Panel()
+        Me.Warna4 = New System.Windows.Forms.Panel()
+        Me.Warna5 = New System.Windows.Forms.Panel()
+        Me.Warna6 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ShapeTepi = New System.Windows.Forms.Panel()
+        Me.ShapeIsian = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Warna7 = New System.Windows.Forms.Panel()
+        Me.Warna8 = New System.Windows.Forms.Panel()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Pilih Mode Gambar"
+        '
+        'CBoxMode
+        '
+        Me.CBoxMode.FormattingEnabled = True
+        Me.CBoxMode.Items.AddRange(New Object() {"Polygon", "PolygonFill", "BrushBebas", "HatchForwardDiagonal", "HatchDottedGrid"})
+        Me.CBoxMode.Location = New System.Drawing.Point(139, 24)
+        Me.CBoxMode.Name = "CBoxMode"
+        Me.CBoxMode.Size = New System.Drawing.Size(103, 21)
+        Me.CBoxMode.TabIndex = 1
+        Me.CBoxMode.Text = "Polygon"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(296, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Ukuran Paint"
+        '
+        'CBoxUkuran
+        '
+        Me.CBoxUkuran.FormattingEnabled = True
+        Me.CBoxUkuran.IntegralHeight = False
+        Me.CBoxUkuran.Items.AddRange(New Object() {"3", "9", "15", "27"})
+        Me.CBoxUkuran.Location = New System.Drawing.Point(382, 24)
+        Me.CBoxUkuran.Name = "CBoxUkuran"
+        Me.CBoxUkuran.Size = New System.Drawing.Size(38, 21)
+        Me.CBoxUkuran.TabIndex = 3
+        Me.CBoxUkuran.Text = "3"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(426, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(18, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "px"
+        '
+        'BtnBersihkan
+        '
+        Me.BtnBersihkan.Location = New System.Drawing.Point(671, 73)
+        Me.BtnBersihkan.Name = "BtnBersihkan"
+        Me.BtnBersihkan.Size = New System.Drawing.Size(75, 23)
+        Me.BtnBersihkan.TabIndex = 5
+        Me.BtnBersihkan.Text = "Bersihkan"
+        Me.BtnBersihkan.UseVisualStyleBackColor = True
+        '
+        'Warna1
+        '
+        Me.Warna1.BackColor = System.Drawing.Color.Red
+        Me.Warna1.Location = New System.Drawing.Point(425, 368)
+        Me.Warna1.Name = "Warna1"
+        Me.Warna1.Size = New System.Drawing.Size(32, 32)
+        Me.Warna1.TabIndex = 7
+        '
+        'Warna2
+        '
+        Me.Warna2.BackColor = System.Drawing.Color.Yellow
+        Me.Warna2.Location = New System.Drawing.Point(463, 368)
+        Me.Warna2.Name = "Warna2"
+        Me.Warna2.Size = New System.Drawing.Size(32, 32)
+        Me.Warna2.TabIndex = 8
+        '
+        'Warna3
+        '
+        Me.Warna3.BackColor = System.Drawing.Color.Lime
+        Me.Warna3.Location = New System.Drawing.Point(503, 368)
+        Me.Warna3.Name = "Warna3"
+        Me.Warna3.Size = New System.Drawing.Size(32, 32)
+        Me.Warna3.TabIndex = 9
+        '
+        'Warna4
+        '
+        Me.Warna4.BackColor = System.Drawing.Color.Blue
+        Me.Warna4.Location = New System.Drawing.Point(541, 368)
+        Me.Warna4.Name = "Warna4"
+        Me.Warna4.Size = New System.Drawing.Size(32, 32)
+        Me.Warna4.TabIndex = 10
+        '
+        'Warna5
+        '
+        Me.Warna5.BackColor = System.Drawing.Color.Fuchsia
+        Me.Warna5.Location = New System.Drawing.Point(579, 368)
+        Me.Warna5.Name = "Warna5"
+        Me.Warna5.Size = New System.Drawing.Size(32, 32)
+        Me.Warna5.TabIndex = 11
+        '
+        'Warna6
+        '
+        Me.Warna6.BackColor = System.Drawing.Color.Cyan
+        Me.Warna6.Location = New System.Drawing.Point(617, 368)
+        Me.Warna6.Name = "Warna6"
+        Me.Warna6.Size = New System.Drawing.Size(32, 32)
+        Me.Warna6.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 377)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Warna Tepi"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(28, 416)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Warna Isi"
+        '
+        'ShapeTepi
+        '
+        Me.ShapeTepi.BackColor = System.Drawing.Color.Black
+        Me.ShapeTepi.Location = New System.Drawing.Point(114, 368)
+        Me.ShapeTepi.Name = "ShapeTepi"
+        Me.ShapeTepi.Size = New System.Drawing.Size(32, 32)
+        Me.ShapeTepi.TabIndex = 8
+        '
+        'ShapeIsian
+        '
+        Me.ShapeIsian.BackColor = System.Drawing.Color.White
+        Me.ShapeIsian.Location = New System.Drawing.Point(114, 406)
+        Me.ShapeIsian.Name = "ShapeIsian"
+        Me.ShapeIsian.Size = New System.Drawing.Size(32, 32)
+        Me.ShapeIsian.TabIndex = 9
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(28, 51)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(621, 308)
+        Me.PictureBox1.TabIndex = 15
+        Me.PictureBox1.TabStop = False
+        '
+        'Warna7
+        '
+        Me.Warna7.BackColor = System.Drawing.Color.Black
+        Me.Warna7.Location = New System.Drawing.Point(425, 406)
+        Me.Warna7.Name = "Warna7"
+        Me.Warna7.Size = New System.Drawing.Size(32, 32)
+        Me.Warna7.TabIndex = 9
+        '
+        'Warna8
+        '
+        Me.Warna8.BackColor = System.Drawing.Color.White
+        Me.Warna8.Location = New System.Drawing.Point(463, 406)
+        Me.Warna8.Name = "Warna8"
+        Me.Warna8.Size = New System.Drawing.Size(32, 32)
+        Me.Warna8.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(757, 450)
+        Me.Controls.Add(Me.Warna8)
+        Me.Controls.Add(Me.Warna7)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ShapeIsian)
+        Me.Controls.Add(Me.ShapeTepi)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Warna6)
+        Me.Controls.Add(Me.Warna5)
+        Me.Controls.Add(Me.Warna4)
+        Me.Controls.Add(Me.Warna3)
+        Me.Controls.Add(Me.Warna2)
+        Me.Controls.Add(Me.Warna1)
+        Me.Controls.Add(Me.BtnBersihkan)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.CBoxUkuran)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CBoxMode)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "MyPaint"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CBoxMode As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CBoxUkuran As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BtnBersihkan As Button
+    Friend WithEvents Warna1 As Panel
+    Friend WithEvents Warna2 As Panel
+    Friend WithEvents Warna3 As Panel
+    Friend WithEvents Warna4 As Panel
+    Friend WithEvents Warna5 As Panel
+    Friend WithEvents Warna6 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ShapeTepi As Panel
+    Friend WithEvents ShapeIsian As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Warna7 As Panel
+    Friend WithEvents Warna8 As Panel
+End Class
---
+++ # Folder Pertemuan 12/MyPaint/Form1.vb
@@ -0,0 +1,201 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Dim modegambar As String
+    Dim warnatepi As Color = Color.Black
+    Dim warnaisian As Color = Color.White
+    Dim tepi As New System.Drawing.Pen(warnatepi, 3)
+    Dim isian As New System.Drawing.SolidBrush(warnaisian)
+    Dim titik As Point = Nothing
+    Dim dipencet As Boolean = False
+    Dim bmp As Bitmap
+    Dim namafile As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BtnBersihkan.PerformClick()
+        tepi.EndCap = LineCap.Round
+    End Sub
+    Private Sub CBoxUkuran_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CBoxUkuran.SelectedIndexChanged
+        tepi.Width = CBoxUkuran.SelectedItem
+    End Sub
+
+    Private Sub CBoxMode_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CBoxMode.SelectedIndexChanged
+        modegambar = CBoxMode.SelectedItem
+    End Sub
+    Private Sub Warna1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna1.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna1.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna1.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+    Private Sub Warna2_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna2.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna2.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna2.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+    Private Sub Warna3_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna3.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna3.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna3.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+    Private Sub Warna4_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna4.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna4.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna4.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+    Private Sub Warna5_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna5.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna5.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna5.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+    Private Sub Warna6_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna6.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna6.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna6.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+    Private Sub Warna7_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna7.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna7.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna7.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+
+    End Sub
+    Private Sub Warna8_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna8.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna8.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna8.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+    Private Sub PictureBox1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseDown
+        tepi.Color = warnatepi
+        isian.Color = warnaisian
+        titik = e.Location
+        dipencet = True
+    End Sub
+    Private Sub PictureBox1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseMove
+        Select Case modegambar
+            Case "BrushBebas"
+                If dipencet Then
+                    Dim pt1, pt2, pt3, pt4 As Point
+                    pt1.X = e.X - (tepi.Width)
+                    pt2.X = e.X + (tepi.Width)
+                    pt1.Y = e.Y
+                    pt2.Y = e.Y
+                    pt3.X = e.X
+                    pt4.X = e.X
+                    pt3.Y = e.Y - (tepi.Width)
+                    pt4.Y = e.Y + (tepi.Width)
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.DrawLine(tepi, pt1, pt2)
+                        g.DrawLine(tepi, pt3, pt4)
+                    End Using
+                    titik = e.Location
+                End If
+            Case "HatchForwardDiagonal"
+                If dipencet Then
+                    Dim hBrush As New HatchBrush(HatchStyle.ForwardDiagonal, warnatepi, warnaisian)
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.FillRectangle(hBrush, e.X, e.Y, tepi.Width, tepi.Width)
+                    End Using
+                End If
+            Case "HatchDottedGrid"
+                If dipencet Then
+                    Dim hBrush As New HatchBrush(HatchStyle.DottedGrid, warnatepi, warnaisian)
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.FillRectangle(hBrush, e.X, e.Y, tepi.Width, tepi.Width)
+                    End Using
+                End If
+        End Select
+        PictureBox1.Invalidate()
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseUp
+        Select Case modegambar
+            Case "Polygon"
+                Dim polygonPoints(5) As Point
+                Dim x As Integer = e.X - titik.X
+                Dim y As Integer = e.Y - titik.Y
+                polygonPoints(0) = New Point(2 / 8 * x + titik.X, e.Y)
+                polygonPoints(1) = New Point(titik.X, 1 / 3 * y + titik.Y)
+                polygonPoints(2) = New Point(4 / 8 * x + titik.X, titik.Y)
+                polygonPoints(3) = New Point(e.X, 1 / 3 * y + titik.Y)
+                polygonPoints(4) = New Point(6 / 8 * x + titik.X, e.Y)
+                polygonPoints(5) = New Point(2 / 8 * x + titik.X, e.Y)
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.DrawPolygon(tepi, polygonPoints)
+                End Using
+            Case "PolygonFill"
+                Dim polygonPoints(5) As Point
+                Dim x As Integer = e.X - titik.X
+                Dim y As Integer = e.Y - titik.Y
+                polygonPoints(0) = New Point(2 / 8 * x + titik.X, e.Y)
+                polygonPoints(1) = New Point(titik.X, 1 / 3 * y + titik.Y)
+                polygonPoints(2) = New Point(4 / 8 * x + titik.X, titik.Y)
+                polygonPoints(3) = New Point(e.X, 1 / 3 * y + titik.Y)
+                polygonPoints(4) = New Point(6 / 8 * x + titik.X, e.Y)
+                polygonPoints(5) = New Point(2 / 8 * x + titik.X, e.Y)
+                Dim b = New Drawing2D.HatchBrush(Drawing2D.HatchStyle.BackwardDiagonal, warnatepi, warnaisian)
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.FillPolygon(isian, polygonPoints)
+                    g.FillPolygon(b, polygonPoints)
+                    g.DrawPolygon(tepi, polygonPoints)
+                End Using
+            Case "BrushBebas"
+                Dim hBrush As New HatchBrush(HatchStyle.OutlinedDiamond, warnatepi, warnaisian)
+                Using g As Graphics = PictureBox1.CreateGraphics()
+                    g.FillEllipse(hBrush, e.X, e.Y, tepi.Width, tepi.Width)
+                End Using
+            Case "HatchForwardDiagonal"
+                Dim hBrush As New HatchBrush(HatchStyle.ForwardDiagonal, warnatepi, warnaisian)
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.FillRectangle(hBrush, e.X, e.Y, tepi.Width, tepi.Width)
+                End Using
+            Case "HatchDottedGrid"
+                Dim hBrush As New HatchBrush(HatchStyle.DottedGrid, warnatepi, warnaisian)
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.FillRectangle(hBrush, e.X, e.Y, tepi.Width, tepi.Width)
+                End Using
+        End Select
+        PictureBox1.Invalidate()
+        dipencet = False
+    End Sub
+    Private Sub btnBersihkan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnBersihkan.Click
+        bmp = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        Using g As Graphics = Graphics.FromImage(bmp)
+            g.Clear(Color.White)
+        End Using
+        PictureBox1.Image = bmp
+    End Sub
+
+
+End Class
